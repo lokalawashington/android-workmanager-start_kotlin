648e93d73d7732d0fbd45ea467a7d3b5e95bda10
Washington <lokalawashington2019@gmail.com>
2023-01-17 12:14:10 +0300

    work manager code lab done

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.background.workers
 
 import android.content.Context
@@ -9,15 +25,18 @@ import androidx.work.Worker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.example.background.KEY_IMAGE_URI
-import com.example.background.R
 
 private const val TAG = "BlurWorker"
 class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
     override fun doWork(): Result {
         val appContext = applicationContext
+
         val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         makeStatusNotification("Blurring image", appContext)
-        // ADD THIS TO SLOW DOWN THE WORKER
+
+        // This is an utility function added to emulate slower work.
         sleep()
 
         return try {
@@ -29,21 +48,19 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
             val resolver = appContext.contentResolver
 
             val picture = BitmapFactory.decodeStream(
-                resolver.openInputStream(Uri.parse(resourceUri)))
+                    resolver.openInputStream(Uri.parse(resourceUri)))
 
             val output = blurBitmap(picture, appContext)
 
-
             // Write bitmap to a temp file
             val outputUri = writeBitmapToFile(appContext, output)
-
-            makeStatusNotification("Output is $outputUri", appContext)
 
             val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
 
             Result.success(outputData)
         } catch (throwable: Throwable) {
             Log.e(TAG, "Error applying blur")
+            throwable.printStackTrace()
             Result.failure()
         }
     }
--- app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
+++ app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.background.workers
 
 import android.content.Context
@@ -19,10 +35,10 @@ import java.util.Locale
 private const val TAG = "SaveImageToFileWorker"
 class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
-    private val title = "Blurred Image"
+    private val Title = "Blurred Image"
     private val dateFormatter = SimpleDateFormat(
-        "yyyy.MM.dd 'at' HH:mm:ss z",
-        Locale.getDefault()
+            "yyyy.MM.dd 'at' HH:mm:ss z",
+            Locale.getDefault()
     )
 
     override fun doWork(): Result {
@@ -35,9 +51,9 @@ class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : Worker(ctx
         return try {
             val resourceUri = inputData.getString(KEY_IMAGE_URI)
             val bitmap = BitmapFactory.decodeStream(
-                resolver.openInputStream(Uri.parse(resourceUri)))
+                    resolver.openInputStream(Uri.parse(resourceUri)))
             val imageUrl = MediaStore.Images.Media.insertImage(
-                resolver, bitmap, title, dateFormatter.format(Date()))
+                    resolver, bitmap, Title, dateFormatter.format(Date()))
             if (!imageUrl.isNullOrEmpty()) {
                 val output = workDataOf(KEY_IMAGE_URI to imageUrl)
 
